c0102bd3072b9259b0789e35774006ce86af6a7b
Miguel Rodrigues <miguel@OrientacoesDockeristicas.ifrn.local>
2026-09-24 15:09:26 -0300

    Rubro negra começando

--- rbng/RN.java
+++ rbng/RN.java
@@ -1,10 +1,51 @@
 package rbng;
 
 public class RN extends Avbp{
+    private int num = 0;
 
     public RN(int key, Object element){
         super(key, element);
         No raiz = root();
         raiz.setcor(0);
+    }
+
+    public void mostrar_cores(){
+        
+        if (size() == 0){
+            System.out.println("Não tem nada para ver");
+        } else {
+            Object matriz[][] = new Object[height(root())+1][size() + 1];
+            visuals(matriz, root());
+            this.num = 0;
+
+            for (int i = 0; i < height(root()) + 1; i++){
+                for (int j = 0; j < size() + 1; j++){
+                    if (matriz[i][j] == null){
+                        System.out.print("  ");
+                    } else {
+                        System.out.print(matriz[i][j] + "  ");
+                    }
+                }
+                System.out.println("");
+            }    
+        }
+        
+    }
+
+    private void visuals(Object[][] ob, No no){
+        if (hasleft(no)){
+            visuals(ob, leftchild(no));
+        }
+        //System.out.println(this.num);
+        if (no.getcor() == 0){
+            ob[depth(no)][this.num] = no.getkey() + " [Negro] ";    
+        } else {
+            ob[depth(no)][this.num] = no.getkey() + " [Rubro] ";
+        }
+        //ob[depth(no)][this.num] = no.getelement() + " [" + no.getfb()+ "]";
+        ++this.num;
+        if (hasright(no)){
+            visuals(ob, rightchild(no));
+        }
     }
 }
--- rbng/Teste.java
+++ rbng/Teste.java
@@ -1,10 +1,18 @@
 package rbng;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.Random;
+
 public class Teste{
     public static void main(String[] args){
         
+        long tempoInicial = System.nanoTime();
+
         Scanner sc = new Scanner(System.in);
         Scanner so = new Scanner(System.in);
+        Random ran = new Random();
 
         int ini = sc.nextInt();
 
@@ -13,7 +21,30 @@ public class Teste{
 
         RN rub = new RN(ini, ini_ele);
 
-        System.out.println(rub.root().getcor());
+        if (rub.root().getcor() == 0){
+            System.out.println("Negro");
+        } else{
+            System.out.println("Rubro");
+        }
+
+        int a = (rub.root()).getkey();
+        int valor;
+        No check;
+        
+        for (int i = 0; i < a; i++){
+            valor = ran.nextInt(a*2);
+            rub.insert(valor, valor);
+        }
+
+        //rub.mostrar_cores();
+        
+        long tempoFinal = System.nanoTime();
+        long tempoTotalNano = tempoFinal - tempoInicial;
+
+        // Converte para milissegundos para facilitar a leitura
+        double tempoTotalMili = tempoTotalNano / 1_000_000.0; 
+
+        System.out.println("Tempo de execução: " + tempoTotalMili + " ms");
     }
 
 }
